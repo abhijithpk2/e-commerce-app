a59f22312cebe726b000b7dea9bff0c50d5dbcd4
Abhijith PK <pkabhijith1@gmail.com>
2023-09-20 22:01:32 +0530

    build folder added

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './client/build')))
-
+ 
 // routes
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/category", categoryRoutes);
